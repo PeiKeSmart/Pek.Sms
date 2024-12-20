46de6c7834255458358c21498d76cbced24304f8
猿人易 <qcjxberin@outlook.com>
2024-12-20 11:19:27 +0800

    添加项目配置文件和初始代码结构
    在代码中进行了以下重要更改：
    
    1. 在 `.editorconfig` 文件中添加了编辑器配置，指定了缩进样式、编码格式、行尾符、代码样式等设置。
    2. 在 `.gitattributes` 文件中添加了默认的行尾规范化行为、命令行 diff 行为、合并驱动程序设置以及图像文件和文档格式的 diff 行为。
    3. 在 `.gitignore` 文件中添加了忽略 Visual Studio 生成的文件和目录的规则。
    4. 添加了 `global.json` 文件，指定了 .NET SDK 的版本和相关设置。
    5. 添加了 `LICENSE` 文件，使用 MIT 许可证。
    6. 添加了 `NuGet.config` 文件，配置了 NuGet 包源。
    7. 添加了 `Pek.Sms.sln` 解决方案文件，包含了项目和解决方案项的配置。
    8. 添加了 `Readme.MD` 文件，描述了 `Pek.Mail` 邮件操作类库。
    9. 添加了 `publish-beta.yml` GitHub Actions 工作流文件，用于在推送到 `main` 分支时构建和发布 beta 版本。
    10. 添加了 `publish.yml` GitHub Actions 工作流文件，用于在推送带有 `v*` 标签时构建和发布正式版本。
    11. 在 `Pek.Sms.Extensions.csproj` 文件中，添加了一个新的项目文件，使用 `Microsoft.NET.Sdk.Web` SDK，设置了目标框架为 `net8.0` 和 `net9.0`，并配置了项目的输出类型、根命名空间、程序集标题、描述、公司、版权、版本等属性，添加了对 `Microsoft.SourceLink.GitHub`、`Pek.NCube.Base` 和 `Pek.NCubeUI` 包的引用，添加了内容文件 `DH.png`，以及对 `Pek.Sms` 项目的引用。
    12. 在 `Pek.Sms.csproj` 文件中，添加了一个新的项目文件，使用 `Microsoft.NET.Sdk` SDK，设置了目标框架为 `net8.0` 和 `net9.0`，并配置了项目的输出类型、根命名空间、程序集标题、描述、公司、版权、版本等属性，添加了对 `Microsoft.SourceLink.GitHub` 和 `Pek.Common` 包的引用，添加了内容文件 `DH.png`。
    13. 在 `SmsSettings.cs` 文件中，添加了 `SmsSettings` 类，用于配置短信设置，包含 `List<SmsData>` 类型的 `Data` 属性，初始化了一些默认的短信数据，添加了 `FindDefault` 和 `FindByCode` 方法，用于根据短信类型和惟一标识获取数据，添加了 `SmsData` 类，用于表示短信数据，包含多个属性，如 `Code`、`Name`、`DisplayName`、`SmsType`、`IsDefault`、`IsEnabled`、`AccessKey`、`AccessSecret`、`PassKey`、`AppId`、`SmsLogin`、`SmsRegister`、`SmsPassword` 等。

---
+++ Pek.Sms/SmsSettings.cs
@@ -0,0 +1,277 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+using Pek.Ids;
+
+namespace Pek.Mail;
+
+/// <summary>短信设置</summary>
+[DisplayName("短信设置")]
+[Config("Sms")]
+public class SmsSettings : Config<SmsSettings>
+{
+    /// <summary>
+    /// 短信数据
+    /// </summary>
+    public List<SmsData> Data { get; set; } = [];
+
+    /// <summary>实例化</summary>
+    public SmsSettings() { }
+
+    /// <summary>加载时触发</summary>
+    protected override void OnLoaded()
+    {
+        if (Data == null || Data.Count == 0)
+        {
+            var list = new List<SmsData>
+            {
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "fenghuo",
+                    DisplayName = "烽火",
+                    SmsType = 0
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "fenghuo",
+                    DisplayName = "烽火",
+                    SmsType = 1
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "fenghuo",
+                    DisplayName = "烽火",
+                    SmsType = 2
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "fenghuo",
+                    DisplayName = "烽火",
+                    SmsType = 3
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "lianlu",
+                    DisplayName = "联麓",
+                    SmsType = 0
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "lianlu",
+                    DisplayName = "联麓",
+                    SmsType = 1
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "lianlu",
+                    DisplayName = "联麓",
+                    SmsType = 2
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "lianlu",
+                    DisplayName = "联麓",
+                    SmsType = 3
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "aliyun",
+                    DisplayName = "阿里云",
+                    SmsType = 0
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "aliyun",
+                    DisplayName = "阿里云",
+                    SmsType = 1
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "aliyun",
+                    DisplayName = "阿里云",
+                    SmsType = 2
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "aliyun",
+                    DisplayName = "阿里云",
+                    SmsType = 3
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "tencent",
+                    DisplayName = "腾讯云",
+                    SmsType = 0
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "tencent",
+                    DisplayName = "腾讯云",
+                    SmsType = 1
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "tencent",
+                    DisplayName = "腾讯云",
+                    SmsType = 2
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "tencent",
+                    DisplayName = "腾讯云",
+                    SmsType = 3
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "mysubmail",
+                    DisplayName = "赛邮云",
+                    SmsType = 0
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "mysubmail",
+                    DisplayName = "赛邮云",
+                    SmsType = 1
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "mysubmail",
+                    DisplayName = "赛邮云",
+                    SmsType = 2
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "mysubmail",
+                    DisplayName = "赛邮云",
+                    SmsType = 3
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "netease",
+                    DisplayName = "网易云信",
+                    SmsType = 0
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "netease",
+                    DisplayName = "网易云信",
+                    SmsType = 1
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "netease",
+                    DisplayName = "网易云信",
+                    SmsType = 2
+                },
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Name = "netease",
+                    DisplayName = "网易云信",
+                    SmsType = 3
+                }
+            };
+
+            Data = list;
+        }
+
+        base.OnLoaded();
+    }
+
+    /// <summary>获取默认的配置数据</summary>
+    public SmsData FindDefault(Int32 SmsType) => Data.FirstOrDefault(e => e.SmsType == SmsType && e.IsDefault) ?? Data[0];
+
+    /// <summary>根据惟一标识获取数据</summary>
+    public SmsData? FindByCode(String Code)
+    {
+        foreach (var item in Data)
+        {
+            if (item.Code == Code) return item;
+        }
+        return null;
+    }
+}
+
+/// <summary>
+/// 短信数据
+/// </summary>
+public class SmsData
+{
+    /// <summary>
+    /// 惟一标识
+    /// </summary>
+    [Description("惟一标识")]
+    public String? Code { get; set; }
+
+    /// <summary>
+    /// 服务商标识
+    /// </summary>
+    [Description("服务商标识")]
+    public String? Name { get; set; }
+
+    /// <summary>
+    /// 显示名称
+    /// </summary>
+    [Description("显示名称")]
+    public String? DisplayName { get; set; }
+
+    /// <summary>
+    /// 短信类型。0为国内通知类，1为国际通知类，2为国内营销类，3为国际营销类
+    /// </summary>
+    [Description("短信类型。0为国内通知类，1为国际通知类，2为国内营销类，3为国际营销类")]
+    public Int32 SmsType { get; set; }
+
+    /// <summary>
+    /// 与短信类型搭配使用，同一类型只能有一个默认
+    /// </summary>
+    [Description("与短信类型搭配使用，同一类型只能有一个默认")]
+    public Boolean IsDefault { get; set; }
+
+    /// <summary>
+    /// 是否启用
+    /// </summary>
+    [Description("是否启用")]
+    public Boolean IsEnabled { get; set; }
+
+    /// <summary>
+    /// Access Key
+    /// </summary>
+    [Description("Access Key")]
+    public String? AccessKey { get; set; }
+
+    /// <summary>
+    /// Access Secret
+    /// </summary>
+    [Description("Access Secret")]
+    public String? AccessSecret { get; set; }
+
+    /// <summary>
+    /// Pass Key
+    /// </summary>
+    [Description("Pass Key")]
+    public String? PassKey { get; set; }
+
+    /// <summary>
+    /// 腾讯短信AppId
+    /// </summary>
+    [Description("腾讯短信AppId")]
+    public String? AppId { get; set; }
+
+    /// <summary>
+    /// 登录短信
+    /// </summary>
+    [Description("登录短信")]
+    public Boolean SmsLogin { get; set; }
+
+    /// <summary>
+    /// 注册短信
+    /// </summary>
+    [Description("注册短信")]
+    public Boolean SmsRegister { get; set; }
+
+    /// <summary>
+    /// 找回密码短信
+    /// </summary>
+    [Description("找回密码短信")]
+    public Boolean SmsPassword { get; set; }
+}
