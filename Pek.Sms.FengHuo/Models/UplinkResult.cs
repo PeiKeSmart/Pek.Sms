f82aa686f7b245fa7c6688d41b9afb2be9fa9ea6
猿人易 <qcjxberin@outlook.com>
2026-01-13 15:24:16 +0800

    新增短信上行推送数据模型 UplinkResult
    新增 UplinkResult.cs，定义 UplinkResult 和 URResult 两个类，用于表示短信上行推送数据及其详细信息，包括手机号、内容、通道号和上行时间。

---
+++ Pek.Sms.FengHuo/Models/UplinkResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Pek.Sms.FengHuo.Models;
+
+/// <summary>
+/// 短信上行推送数据
+/// </summary>
+public class UplinkResult
+{
+    public Int32 total { get; set; }
+
+    public IList<URResult>? reports { get; set; }
+}
+
+public class URResult
+{
+    public String? user_mobile { get; set; }
+
+    public String? user_content { get; set; }
+
+    public String? channel_num { get; set; }
+
+    public DateTime mo_time { get; set; }
+}
